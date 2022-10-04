75c79c2528efb48ff194de3b93b6f88d37d16c73
Balu-Rama-Chandra <41617154+Balurc@users.noreply.github.com>
2022-10-04 21:00:34 +0700

    Add files via upload

---
+++ week4_analytics_engineering/models/staging/stg_fvh_trip_data.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as string) as vendorid,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime
+
+    -- trip info
+    -- sr_flag,
+from {{ source('staging','external_fhv_tripdata') }}
+where dispatching_base_num is not null 
